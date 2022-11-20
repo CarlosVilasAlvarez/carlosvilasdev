14274605e5dcbd0d56eb4d8765d7510c89d44843
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2022-11-20 03:19:52 +0100

    add post frontmatter interface

---
+++ packages/website/src/types.ts
@@ -0,0 +1,12 @@
+export interface Frontmatter {
+    title: string;
+    pubDate: string;
+    description: string;
+    author: string;
+    image: {
+        url: string;
+        alt: string;
+    };
+    tags: string[];
+    draft: boolean;
+}
